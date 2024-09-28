c6aa5a4d5637142a22656f00dd96c045bfb6bb45
Fatih <fatihcicek896@gmail.com>
2024-09-28 19:10:16 +0300

    add template

---
+++ WebApplication1/Controllers/UrunController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class UrunController : Controller
+    {
+        // GET: DefauUrunlt
+        public string Index()
+        {
+            return "URUN ";
+        }
+        public ViewResult Liste()
+        {
+            return View();
+        }
+    }
+}
