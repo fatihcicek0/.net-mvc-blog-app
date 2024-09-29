75345d5a59787b12dc9f79b04803e88a57d330b8
Fatih <fatihcicek896@gmail.com>
2024-09-29 22:22:23 +0300

    add layout 2

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -21,5 +21,9 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+        public ActionResult Faq()
+        {
+            return View();
+        }
     }
 }
